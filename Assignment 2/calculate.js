6de771798e2f7f3b0c1a66fca2cc9cee8aa0a043
Daniel <dgt1@sfu.ca>
2023-10-24 19:12:16 -0700

    Sliders and file change updates histogram dynamically, need to work on styling

--- Assignment 2/calculate.js
+++ Assignment 2/calculate.js
@@ -44,7 +44,6 @@ function gradeReader(event, callback) {
 
             //Take the data from the right column, after the first column
             const rows = contents.split('\r');
-            const grades = [];
             for (let i = 1; i < rows.length; i++) {
                 const columns = rows[i].split(",");
                 //If the data file contains more than one column, get the second column
@@ -59,39 +58,47 @@ function gradeReader(event, callback) {
 }
 
 function gradeSorter(){
+    APlusList.length=0;
+    AList.length=0;
+    AMinusList.length=0;
+    BPlusList.length=0;
+    BList.length=0;
+    BMinusList.length=0;
+    CPlusList.length=0;
+    CList.length=0;
+    CMinusList.length=0;
+    DList.length=0;
+    FList.length=0;
     for(let i=0; i<grades.length; i++){
         let currentGrade = grades[i];
-        if(sliders[1].value < Number(currentGrade)){
+        if(sliders[1].value <= Number(currentGrade)){
             APlusList.push(currentGrade);
         }
-        else if(sliders[2].value < Number(currentGrade)){
+        else if(sliders[2].value <= Number(currentGrade)){
             AList.push(currentGrade);
         }
-        else if(sliders[3].value < Number(currentGrade)){
-            APlusList.push(currentGrade);
-        }
-        else if(sliders[4].value < Number(currentGrade)){
+        else if(sliders[3].value <= Number(currentGrade)){
             AMinusList.push(currentGrade);
         }
-        else if(sliders[5].value < Number(currentGrade)){
+        else if(sliders[4].value <= Number(currentGrade)){
             BPlusList.push(currentGrade);
         }
-        else if(sliders[6].value < Number(currentGrade)){
+        else if(sliders[5].value <= Number(currentGrade)){
             BList.push(currentGrade);
         }
-        else if(sliders[7].value < Number(currentGrade)){
+        else if(sliders[6].value <= Number(currentGrade)){
             BMinusList.push(currentGrade);
         }
-        else if(sliders[8].value < Number(currentGrade)){
+        else if(sliders[7].value <= Number(currentGrade)){
             CPlusList.push(currentGrade);
         }
-        else if(sliders[9].value < Number(currentGrade)){
+        else if(sliders[8].value <= Number(currentGrade)){
             CList.push(currentGrade);
         }
-        else if(sliders[10].value < Number(currentGrade)){
+        else if(sliders[9].value <= Number(currentGrade)){
             CMinusList.push(currentGrade);
         }
-        else if(sliders[11].value < Number(currentGrade)){
+        else if(sliders[10].value <= Number(currentGrade)){
             DList.push(currentGrade);
         }
         else{
@@ -101,7 +108,15 @@ function gradeSorter(){
 }
 
 function updateHistogram(){
-    bars[0].style.width = ((APlusList.length/grades.length)*100)+1+"%";
-    console.log(((APlusList.length/grades.length)*100)+1+"%")
-
+    bars[0].style.width = ((APlusList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[1].style.width = ((AList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[2].style.width = ((AMinusList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[3].style.width = ((BPlusList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[4].style.width = ((BList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[5].style.width = ((BMinusList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[6].style.width = ((CPlusList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[7].style.width = ((CList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[8].style.width = ((CMinusList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[9].style.width = ((DList.length/grades.length)*sliders[0].value)+1+"%";
+    bars[10].style.width = ((FList.length/grades.length)*sliders[0].value)+1+"%";
 }
