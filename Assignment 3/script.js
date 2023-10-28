b11ab6e90064c02d0b3b7b0747660c22daf0b0b3
Daniel Todd <dgtutv@gmail.com>
2023-10-27 19:17:41 -0700

    Working on dynamic elements in table

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+const addPigButton = document.querySelector("button");
+const addPigTable = document.querySelector("#addPigTable");
+const newPigCategory = document.querySelector("#newPigCategory");
+let tableShowing = false;
+addPigButton.addEventListener('click', function (e) {
+    if (tableShowing) {
+        addPigTable.style.display = 'none';
+    }
+    else {
+        addPigTable.style.display = 'table';
+    }
+    tableShowing = !tableShowing;
+});
+newPigCategory.addEventListener("change", function (e) {
+    const category = newPigCategory.value;
+    if (category == "Select a category") {
+        const dynamicElements = document.querySelectorAll(".dynamic");
+        for (const element of dynamicElements) {
+            const currentElement = element;
+            if (!currentElement.classList.contains("hidden")) {
+                currentElement.classList.add("hidden");
+            }
+        }
+    }
+    else if (category == "black") {
+        const blackPigDynamic1 = document.querySelector("#blackPigDynamic1");
+        const blackPigDynamic2 = document.querySelector("#blackPigDynamic2");
+        blackPigDynamic1.classList.toggle("hidden");
+        blackPigDynamic2.classList.toggle("hidden");
+    }
+});
--- Assignment 3/script.ts
+++ Assignment 3/script.ts
@@ -1,1 +1,33 @@
-console.log("hello world");
+const addPigButton: HTMLButtonElement = document.querySelector("button");
+const addPigTable: HTMLTableElement = document.querySelector("#addPigTable");
+const newPigCategory: HTMLSelectElement = document.querySelector("#newPigCategory");
+
+let tableShowing: boolean = false;
+addPigButton.addEventListener('click', function(e){
+    if(tableShowing){
+        addPigTable.style.display =  'none';
+    }
+    else{
+        addPigTable.style.display = 'table';
+    }
+    tableShowing = !tableShowing;
+});
+
+newPigCategory.addEventListener("change", function(e){
+    const category: string = newPigCategory.value;
+    if(category == "Select a category"){
+        const dynamicElements: NodeListOf<HTMLTableRowElement> = document.querySelectorAll(".dynamic");
+        for(const element of dynamicElements){
+            const currentElement: HTMLTableRowElement = element as HTMLTableRowElement;
+            if(!currentElement.classList.contains("hidden")){
+                currentElement.classList.add("hidden");
+            }
+        }
+    }
+    else if(category == "black"){
+        const blackPigDynamic1: HTMLTableRowElement = document.querySelector("#blackPigDynamic1");
+        const blackPigDynamic2: HTMLTableRowElement = document.querySelector("#blackPigDynamic2");
+        blackPigDynamic1.classList.toggle("hidden");
+        blackPigDynamic2.classList.toggle("hidden");
+    }
+});
