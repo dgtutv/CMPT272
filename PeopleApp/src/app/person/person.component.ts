db87d9ecd87cf66c76961bdd048ae1006542216e
Daniel <dgtutv@gmail.com>
2023-11-15 10:28:37 -0800

    Fixed code that was accidentally modified

--- PeopleApp/src/app/person/person.component.ts
+++ PeopleApp/src/app/person/person.component.ts
@@ -1,21 +1,21 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
 })
 export class PersonComponent {
-  @Input() report: any; //Tells typescirpt this is a input from another component
+  @Input() person: any; //Tells typescirpt this is a input from another component
   @Output() delete = new EventEmitter;  //Event emitters are used to call events (click, mouseover, keypress, etc.,)
 
   constructor(){
 
   }
 
-  onDelete(event: any, reportToDelete: string){  //Ran when delete clicked (via the HTML Angular event listener)
+  onDelete(event: any, personToDelete: string){  //Ran when delete clicked (via the HTML Angular event listener)
     //People list keeps track of people, so we invoke the people-list component
-    event['delete-report'] = reportToDelete;    //We are assuming all of the people are unique
+    event['delete-person'] = personToDelete;    //We are assuming all of the people are unique
     this.delete.emit(event);  //Send to list component
   }
 }
