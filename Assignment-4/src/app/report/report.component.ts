a47e72b0f023b214909fadb96021d88419b731b2
Daniel <dgtutv@gmail.com>
2023-11-15 11:36:29 -0800

    Working on report component

--- Assignment-4/src/app/report/report.component.ts
+++ Assignment-4/src/app/report/report.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-report',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent {
+  @Input() report: any; //Tells typescirpt this is a input from another component
+  @Output() delete = new EventEmitter;  //Event emitters are used to call events (click, mouseover, keypress, etc.,)
 
+  constructor(){
+
+  }
+
+  onDelete(event: any, reportToDelete: string){  //Ran when delete clicked (via the HTML Angular event listener)
+    event['delete-report'] = reportToDelete;    //We are assuming all of the reports are unique
+    this.delete.emit(event);  //Send to list component
+  }
 }
