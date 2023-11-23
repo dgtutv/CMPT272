061091b8341c4f892a2e1a392acbb91a6f3613b0
Daniel Todd <dgtutv@gmail.com>
2023-11-22 20:53:58 -0800

    Renamed report service

--- NightCrusadeTitans/src/app/report.service.ts
+++ NightCrusadeTitans/src/app/report.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ReportServicerService {
+export class ReportService {
   reports: Array<Report>
   constructor() {
     this.reports = this.pull();
