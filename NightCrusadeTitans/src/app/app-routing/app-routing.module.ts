5511ecb7e3c7070524edecaec0379051c2330a61
Daniel Todd <dgtutv@gmail.com>
2023-11-29 20:08:01 -0800

    Dynamix styling of report page, removed most wanted page

--- NightCrusadeTitans/src/app/app-routing/app-routing.module.ts
+++ NightCrusadeTitans/src/app/app-routing/app-routing.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from '../home-page/home-page.component';
 import { ReportPageComponent } from '../report-page/report-page.component';
-import { MostWantedPageComponent } from '../most-wanted-page/most-wanted-page.component';
 import { InfoPageComponent } from '../info-page/info-page.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../app.component';
@@ -11,7 +10,6 @@ import { AppComponent } from '../app.component';
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'report', component: ReportPageComponent },
-  { path: 'most-wanted', component: MostWantedPageComponent },
   { path: 'info', component: InfoPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' } 
--- NightCrusadeTitans/src/app/app.module.ts
+++ NightCrusadeTitans/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { ReportListComponent } from './report-list/report-list.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { MapComponent } from './map/map.component';
-import { MostWantedPageComponent } from './most-wanted-page/most-wanted-page.component';
 import { ReportPageComponent } from './report-page/report-page.component';
 import { InfoPageComponent } from './info-page/info-page.component';
 
@@ -27,7 +26,6 @@ import { HttpClientModule } from '@angular/common/http';
     HomePageComponent,
     HeaderComponent,
     MapComponent,
-    MostWantedPageComponent,
     ReportPageComponent,
     InfoPageComponent,
     ReportComponent,
