a712aa0437a3fb693e81f1de0089cd17608a4947
Daniel Todd <dgtutv@gmail.com>
2023-12-03 19:23:48 -0800

    Remove unused imports and update todo.txt

--- NightCrusadeTitans/src/app/app.component.ts
+++ NightCrusadeTitans/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
